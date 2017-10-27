0a6997cb37e4fad3730fc6d10b9fc6c9fe19dea3
deepthiaks <dvenkatesh@unomaha.edu>
2017-10-27 10:44:57 -0500

    modified admin.py

--- practice/hackPracs/chroniFic/admin.py
+++ practice/hackPracs/chroniFic/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-
-from django.contrib import admin
 from .models import Hospital, Department, Specialist, FAQ
 
 
@@ -8,9 +6,25 @@ class HospitalList(admin.ModelAdmin):
     list_display = ('hospital_id', 'hospital_name', 'city')
     list_filter = ('hospital_id', 'hospital_name', 'city')
     search_fields = ('hospital_id', 'hospital_name')
-    ordering = ['hospital_id']
+    ordering = ['hospital_id','hospital_name']
 
+class DepartmentList(admin.ModelAdmin):
+    list_display = ('department_id','department_name')
+    list_filter = ('department_id','department_name')
+    search_fields = ('department_id','department_name')
+    ordering = ['department_id','department_name']
 
+class SpecialistList(admin.ModelAdmin):
+    list_display = ('specialist_id','specialist_name','state')
+    list_filter = ('specialist_id','specialist_name')
+    search_fields = ('specialist_id','specialist_name','state')
+    ordering = ['specialist_id','specialist_name']
+
+class FAQList(admin.ModelAdmin):
+    list_display = ('faq_id')
+    list_filter = ('faq_id')
+    search_fields=('faq_id')
+    ordering = ['faq_id']
 
 admin.site.register(Hospital, HospitalList)
 admin.site.register(Department)
--- practice/hackPracs/chroniFic/views.py
+++ practice/hackPracs/chroniFic/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import *
+from django.shortcuts import render, get_object_or_404
+
 
 # Create your views here.
 
@@ -11,3 +14,11 @@ def home(request):
 def login(request):
    return render(request, 'portfolio/login.html',
                  {'portfolio': login})
+
+
+def department_view(request,pk):
+    department= get_object_or_404(Department,pk=pk)
+    deoartment = Department.objects.filter()
+    customers = Customer.objects.filter(created_date__lte=timezone.now())
+    investments =Investment.objects.filter(customer=pk)
+    return render(request,'hackpracs/department.html')
